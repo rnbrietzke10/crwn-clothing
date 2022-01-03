171b10d0409aeccecc9d4ec92e51fe11639f31d1
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-01-02 19:57:11 -0600

    add checkout page, checkout item component and update cart to use selectors from reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,11 +6,11 @@ import { withRouter } from "react-router-dom";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems, history }) => {
-  console.log(history);
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -25,6 +25,7 @@ const CartDropdown = ({ cartItems, history }) => {
       <CustomButton
         onClick={() => {
           history.push("/checkout");
+          dispatch(toggleCartHidden());
         }}
       >
         GO TO CHECKOUT

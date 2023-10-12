898f2eeec34056776f8f4ad36eea009a65f890e4
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-10-12 14:30:29 -0500

    add navigation to category items in directory to go to shop pages

--- src/components/DirectoryItem/DirectoryItem.js
+++ src/components/DirectoryItem/DirectoryItem.js
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import {
   BackgroundImage,
   Body,
@@ -5,10 +7,13 @@ import {
 } from './DirectoryItem.styles';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
-      <BackgroundImage imageUrl={imageUrl} />
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageurl={imageUrl} />
       <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
